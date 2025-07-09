61c6f86a553ac7b85dbf2765dff2fbb9b2529d29
Louis Ouellet <louis_ouellet@hotmail.com>
2025-07-09 15:54:09 -0400

    General: Code Standardization & Clean-up

--- Endpoint.php
+++ Endpoint.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Core Framework - LoggerEndpoint
- *
- * @license    MIT (https://mit-license.org/)
- * @author     Louis Ouellet <louis@laswitchtech.com>
- */
-
 // Import additionnal class into the global namespace
 use \LaswitchTech\Core\Objects;
 use \LaswitchTech\Core\Abstracts\Endpoint;
