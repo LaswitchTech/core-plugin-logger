618017f00858ef381475a90186241840b5e9145c
Louis Ouellet <louis_ouellet@hotmail.com>
2025-09-23 11:58:16 -0400

    BUGFIX: library.js:2 Uncaught TypeError: Cannot read properties of undefined (reading 'logger')

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
 API.success(function(response, endpoint){
+    if(typeof response.app === 'undefined') return;
+    if(typeof response.app.logLevel === 'undefined') return;
     API.debug((response.app.logLevel >= 5));
 });
